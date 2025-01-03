66ae211a784a8ad4f32d2cda2f941069e714f0fc
Abolfazl Tabe Bordbar <77238230+Abolfazl-Tabe-Bordbar@users.noreply.github.com>
2025-01-03 11:52:15 +0330

    ed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const OTPInput = () => {
 
         // بررسی اینکه پیامک حاوی کد OTP است
         if (otpCredential && otpCredential.code) {
-          
+
           alert(otpCredential.code)
           setOtp(otpCredential.code); // کد OTP را تنظیم کنید
         } else {
@@ -42,14 +42,15 @@ const OTPInput = () => {
     <div className='mx-auto flex flex-col justify-center items-center  w-32 h-32'>
       <h2>Test OTP</h2>
       <span>OTP is {otp}</span>
-      {/* <input
+      <input
       className='bg-gray-100 h-10 rounded-md text-white text-center'
         type="text"
-        value={otp}
-        onChange={(e) => setOtp(e.target.value)}
+        name='code'
+        // value={otp}
+        // onChange={(e) => setOtp(e.target.value)}
         placeholder="Enter OTP"
         // readOnly // فقط خواندنی چون به صورت خودکار پر می‌شود
-      /> */}
+      />
     </div>
   );
 };
